85649ac4bb54f791941c2dbd76baec22c668a66f
eazima <nimo07412@gmail.com>
2023-10-14 16:55:53 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Retrn: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
