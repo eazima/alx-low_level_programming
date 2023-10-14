cafaf8830856eb0368e61b22118f331affa9a7cc
eazima <nimo07412@gmail.com>
2023-10-14 16:26:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ * Return: Always (sucess)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
